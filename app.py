17a5ca4495fec7472286c972add90a651b1e903f
Zoë Webb-Mack <zoewebb-mack@Zoes-MacBook-Air-3.local>
2020-05-13 10:26:22 -0700

    Third commit: added comments to app, added image to home page (image needs work)

--- app.py
+++ app.py
@@ -14,23 +14,30 @@ app = Flask(__name__)
 #static route
 @app.route("/")
 def hello():
+    """Function runs html for home page, which includes personal information
+    and links to other pages"""
     #return "Hello World!"
     return render_template("index.html")
-
 ##need to edit to add personal information, links to other pages
+    #fix image, need to rotate and resize
+    #still need doctstrings
 
-#optional test
+#define routes
 @app.route("/1006")
 def ten_oh_six():
+    """Function runs html for test 1006 page"""
     return render_template("1006.html")
-    #server error
     
 @app.route("/classes")
 def classes():
+    """Function runs html for classes page, which includes information
+    about my classes this semester and my favorite class"""
     return render_template("classes.html")
 
 @app.route("/assignments")
 def assignments():
+    """Function runs html for assignments page, which includes information about
+    our assignements in ENGI1006 and my favorite assignment"""
     return render_template("assignments.html")
 
 
@@ -40,5 +47,3 @@ if __name__ == "__main__":
     
 #NOTES
     #20:00 to link to other sections of website
-    #23:00 git start
-    #36:00 github repository
